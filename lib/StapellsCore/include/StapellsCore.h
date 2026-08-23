b2c8bd3bb510bd764f162fe50268d6e0c5536359
cstapells <cstapells@gmail.com>
2026-08-23 18:35:47 -0400

    Implement Base service: lib/StapellsCore/include/StapellsCore.h

--- lib/StapellsCore/include/StapellsCore.h
+++ lib/StapellsCore/include/StapellsCore.h
@@ -2,8 +2,12 @@
 
 #include <Arduino.h>
 
+#include "stapells/ConfigStore.h"
 #include "stapells/HealthStrip.h"
+#include "stapells/MqttService.h"
+#include "stapells/NetworkService.h"
 #include "stapells/Platform.h"
+#include "stapells/WebService.h"
 
 namespace stapells {
 
@@ -22,17 +26,36 @@ class Core {
  public:
   void begin();
   void loop();
-
   void activity();
-  void setState(NodeState state);
-  NodeState state() const { return state_; }
 
+  NodeState state() const { return state_; }
   const PlatformInfo& platform() const { return platform_; }
+  const CoreConfig& config() const { return configStore_.config(); }
 
  private:
+  static Core* instance_;
+  static void onMqttMessage(const String& topic, const String& payload);
+  static String statusJson();
+  static bool saveConfigJson(const String& body, String& error);
+  static void reboot();
+  static void factoryReset();
+
+  void handleMqttMessage(const String& topic, const String& payload);
+  void updateState();
+  void setState(NodeState state);
+  void publishStatus(bool force = false);
+  const char* stateName() const;
+
   PlatformInfo platform_{};
+  ConfigStore configStore_{};
   HealthStrip health_{};
+  NetworkService network_{};
+  MqttService mqtt_{};
+  WebService web_{};
   NodeState state_{NodeState::Booting};
+  bool started_{false};
+  bool timeStarted_{false};
+  uint32_t lastStatusMs_{0};
 };
 
 }  // namespace stapells
