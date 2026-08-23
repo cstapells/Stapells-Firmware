1335b6426de1f39e1048a97e1ff78c03fbfe7271
cstapells <cstapells@gmail.com>
2026-08-23 18:51:35 -0400

    Remove AP and ElegantOTA: lib/StapellsCore/include/stapells/WebService.h

--- lib/StapellsCore/include/stapells/WebService.h
+++ lib/StapellsCore/include/stapells/WebService.h
@@ -13,30 +13,17 @@ using StapellsWebServer = WebServer;
 namespace stapells {
 
 using StatusProvider = String (*)();
-using ConfigWriter = bool (*)(const String& body, String& error);
-using VoidAction = void (*)();
-using BoolAction = void (*)(bool value);
-
 class WebService {
  public:
   WebService();
-  void begin(StatusProvider statusProvider, ConfigWriter configWriter,
-             VoidAction rebootAction, VoidAction factoryResetAction,
-             VoidAction otaStartAction, BoolAction otaEndAction);
+  void begin(StatusProvider statusProvider);
   void loop();
 
  private:
   void sendHome();
   void sendStatus();
-  void saveConfig();
-
   StapellsWebServer server_;
   StatusProvider statusProvider_{nullptr};
-  ConfigWriter configWriter_{nullptr};
-  VoidAction rebootAction_{nullptr};
-  VoidAction factoryResetAction_{nullptr};
-  VoidAction otaStartAction_{nullptr};
-  BoolAction otaEndAction_{nullptr};
 };
 
 }  // namespace stapells
