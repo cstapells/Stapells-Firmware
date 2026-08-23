88cc9a75e5c50bc515729e3d2dc28cb4750c84b8
cstapells <cstapells@gmail.com>
2026-08-23 18:51:32 -0400

    Remove AP and ElegantOTA: lib/StapellsCore/include/stapells/NetworkService.h

--- lib/StapellsCore/include/stapells/NetworkService.h
+++ lib/StapellsCore/include/stapells/NetworkService.h
@@ -12,19 +12,16 @@ class NetworkService {
   void loop();
 
   bool stationConnected() const;
-  bool setupAccessPointActive() const { return apActive_; }
   String ipAddress() const;
   int32_t rssi() const;
 
  private:
   void connectStation();
-  void startSetupAccessPoint();
-
   const CoreConfig* config_{nullptr};
   String boardId_{};
-  bool apActive_{false};
   uint32_t attemptStartedMs_{0};
   uint32_t nextAttemptMs_{0};
 };
 
 }  // namespace stapells
+
