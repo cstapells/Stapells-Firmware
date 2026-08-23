ab0e6abe6f63306fb78b16bb0af067c425fa0a99
cstapells <cstapells@gmail.com>
2026-08-23 18:35:50 -0400

    Add Base service: lib/StapellsCore/include/stapells/MqttService.h

---
+++ lib/StapellsCore/include/stapells/MqttService.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <Arduino.h>
+#include <PubSubClient.h>
+
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>
+#else
+#include <WiFi.h>
+#endif
+
+#include "stapells/ConfigStore.h"
+
+namespace stapells {
+
+using MqttMessageHandler = void (*)(const String& topic, const String& payload);
+
+class MqttService {
+ public:
+  MqttService();
+  void begin(const CoreConfig& config, const String& boardId,
+             MqttMessageHandler handler);
+  void loop(bool networkReady);
+  bool connected() const { return client_.connected(); }
+  bool publish(const String& suffix, const String& payload, bool retained = false);
+  String topic(const String& suffix) const;
+
+ private:
+  static MqttService* instance_;
+  static void callback(char* topic, uint8_t* payload, unsigned int length);
+  void connect();
+
+  WiFiClient transport_{};
+  PubSubClient client_;
+  const CoreConfig* config_{nullptr};
+  String boardId_{};
+  MqttMessageHandler handler_{nullptr};
+  uint32_t nextAttemptMs_{0};
+};
+
+}  // namespace stapells
