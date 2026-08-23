a9f35570caa093156b46d52447adb01ee5830321
cstapells <cstapells@gmail.com>
2026-08-23 18:35:53 -0400

    Implement Base service: lib/StapellsCore/include/stapells/Platform.h

--- lib/StapellsCore/include/stapells/Platform.h
+++ lib/StapellsCore/include/stapells/Platform.h
@@ -4,22 +4,20 @@
 
 namespace stapells {
 
-enum class PlatformFamily : uint8_t {
-  Esp8266,
-  Esp32,
-  Esp32C3,
-  Esp32S3,
-  Unknown
-};
+enum class PlatformFamily : uint8_t { Esp8266, Esp32, Esp32C3, Esp32S3, Unknown };
 
 struct PlatformInfo {
   PlatformFamily family{PlatformFamily::Unknown};
   String name{"unknown"};
   String macAddress{};
   String boardId{};
+  String resetReason{};
+  uint32_t flashSize{0};
 };
 
 PlatformInfo detectPlatform();
+uint32_t freeHeap();
 void platformYield();
+void restartPlatform();
 
 }  // namespace stapells
